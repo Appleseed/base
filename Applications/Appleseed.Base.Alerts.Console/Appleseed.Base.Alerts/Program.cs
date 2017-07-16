76a6c696f39da95cbb6f966a1f2f8d22e0a49800
Eric Ramseur <ramseur@anant.us>
2017-07-15 23:51:04 -0400

    solr url

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Program.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Program.cs
@@ -70,6 +70,7 @@ namespace Appleseed.Base.Alerts
 
         static string Mode = System.Configuration.ConfigurationManager.AppSettings["Mode"];
         static string TestEmail = System.Configuration.ConfigurationManager.AppSettings["TestEmail"];
+        static string SolrURL = System.Configuration.ConfigurationManager.AppSettings["SolrURL"];
 
 
 
