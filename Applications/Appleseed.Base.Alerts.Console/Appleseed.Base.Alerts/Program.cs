1375b50f90e18265932a94aca82f159e56ac7de8
Eric Ramseur <ramseur@anant.us>
2017-07-15 23:44:16 -0400

    push of program

---
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using Dapper;
+
+namespace Appleseed.Base.Alerts
+{
+  
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        
+
+        #region helpers
+        static string UppercaseFirst(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            char[] a = s.ToCharArray();
+            a[0] = char.ToUpper(a[0]);
+            return new string(a);
+        }
+        #endregion
+    }
+}
