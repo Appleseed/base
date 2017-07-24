cb935af027dce5e06f099483b4754288229c1276
Eric Ramseur <ramseur@anant.us>
2017-07-23 23:38:40 -0400

    add foundation for last method in provider

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Providers/EmailAlertProvider.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Providers/EmailAlertProvider.cs
@@ -11,6 +11,8 @@ using System.Configuration;
 using System.Net;
 using System.IO;
 using Newtonsoft.Json;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace Appleseed.Base.Alerts.Providers
 {
@@ -18,6 +20,13 @@ namespace Appleseed.Base.Alerts.Providers
     {
         static IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
         static string SolrURL = System.Configuration.ConfigurationManager.AppSettings["SolrURL"];
+
+        static string MailSubject = System.Configuration.ConfigurationManager.AppSettings["MailSubject"];
+        static string MailHeaderText = System.Configuration.ConfigurationManager.AppSettings["MailHeaderText"];
+        static string MailFrom = System.Configuration.ConfigurationManager.AppSettings["MailFrom"];
+        static string MailFromName = System.Configuration.ConfigurationManager.AppSettings["MailFromName"];
+
+        static string APIKey = System.Configuration.ConfigurationManager.AppSettings["SendGridAPIKey"];
 
         public List<UserAlert> GetUserAlertSchedules(string scheudle)
         {
@@ -48,6 +57,53 @@ namespace Appleseed.Base.Alerts.Providers
 
             }
 
+        }
+
+        public async Task SendAlert(string email, string link, RootSolrObject results)
+        {
+
+            if (results != null && results.response != null && results.response.docs != null && results.response.docs.Count() > 0)
+            {
+                var client = new SendGridClient(APIKey);
+                var from = new EmailAddress(MailFrom, MailFromName);
+                var subject = MailSubject;
+                var to = new EmailAddress(email, null);
+
+                var plainTextContent = " ";
+
+                StringBuilder sbHtmlContent = new StringBuilder();
+
+                //Header
+                sbHtmlContent.Append("<h1>" + MailHeaderText + "</h1>");
+                sbHtmlContent.Append("<br/><br/>");
+
+                for (int i = 0; i < results.response.docs.Count(); i++)
+                {
+                    sbHtmlContent.Append("<br/><br/>");
+
+                    sbHtmlContent.Append("<br/><h2>" + UppercaseFirst(results.response.docs[i].item_type[0]) + " : " + results.response.docs[i].recall_number[0] + "</h2>");
+                    sbHtmlContent.Append("<strong>Status: </strong>" + results.response.docs[i].status[0] + "<br/>");
+                    sbHtmlContent.Append("<strong>Classification: </strong>" + results.response.docs[i].classification + "<br/>");
+                    sbHtmlContent.Append("<strong>Description: </strong>" + results.response.docs[i].product_description[0] + "<br/>");
+                    sbHtmlContent.Append("<strong>Code Info: </strong>" + results.response.docs[i].code_info[0] + "<br/>");
+                    sbHtmlContent.Append("<strong>Recall Reason: </strong> " + results.response.docs[i].reason_for_recall[0] + "<br/>");
+                    sbHtmlContent.Append("<strong>Voluntary Mandated: </strong> " + results.response.docs[i].voluntary_mandated[0] + "<br/>");
+                    sbHtmlContent.Append("<strong>Product Quantity: </strong> " + results.response.docs[i].reason_for_recall[0] + "<br/>");
+                    sbHtmlContent.Append("<strong>Recalling Firm: </strong> " + results.response.docs[i].recalling_firm + "<br/>");
+                    sbHtmlContent.Append("<strong>Recalling Firm Address: </strong> " + results.response.docs[i].address_1[0] + "<br/>");
+
+
+                    //sbHtmlContent.Append("Report Date : " + results.response.docs[i].report_date + "<br/>");
+                }
+
+                //Footer
+                sbHtmlContent.Append("<br/><br/>");
+                sbHtmlContent.Append("<a href=" + link + "> More Search Results </a>");
+
+                var htmlContent = sbHtmlContent.ToString();
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                var response = await client.SendEmailAsync(msg);
+            }
         }
     }
 }
