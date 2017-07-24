72b37f49f4fbbfb283cad6462a05fc725e9f96a2
Eric Ramseur <ramseur@anant.us>
2017-07-23 23:16:51 -0400

    refactor all models out

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Program.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Program.cs
@@ -19,39 +19,6 @@ namespace Appleseed.Base.Alerts
 {
   
 
-  
-    
-    class SolrResponseItem
-
-    {
-        public string id { get; set; }
-        public string[] item_type { get; set; }
-
-        public string[] address_1 { get; set; }
-
-        public string city { get; set; }
-
-        public string state { get; set; }
-        public string classification { get; set; }
-        public string country { get; set; }
-        public string[] postal_code { get; set; }
-
-        public string[] product_description { get; set; }
-        public string[] product_quantity { get; set; }
-        public string[] product_type { get; set; }
-        public string[] code_info { get; set; }
-
-        public string[] reason_for_recall { get; set; }
-        public DateTime recall_initiation_date { get; set; }
-        public string[] recall_number { get; set; }
-        public string recalling_firm { get; set; }
-        public string[] voluntary_mandated { get; set; }
-
-        public DateTime report_date { get; set; }
-        public string[] status { get; set; }
-
-
-    }
     class Program
     {
         #region App Config Values
