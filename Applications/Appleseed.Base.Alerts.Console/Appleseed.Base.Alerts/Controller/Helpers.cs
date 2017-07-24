2f64d176af1ea9c552b1173dccdc2a2f9652f835
Eric Ramseur <ramseur@anant.us>
2017-07-23 23:41:25 -0400

    add public to method

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Controller/Helpers.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Controller/Helpers.cs
@@ -8,5 +8,17 @@ namespace Appleseed.Base.Alerts.Controller
 {
     class Helpers
     {
+        #region helpers
+        public static string UppercaseFirst(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            char[] a = s.ToCharArray();
+            a[0] = char.ToUpper(a[0]);
+            return new string(a);
+        }
+        #endregion
     }
 }
