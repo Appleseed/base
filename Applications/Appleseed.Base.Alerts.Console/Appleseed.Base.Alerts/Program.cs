f9d98786e506e765576ad83bc31e42062fa9b3d9
Eric Ramseur <ramseur@anant.us>
2017-07-23 22:08:26 -0400

    add of mail schedule

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Program.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Program.cs
@@ -79,7 +79,9 @@ namespace Appleseed.Base.Alerts
         static string MailHeaderText = System.Configuration.ConfigurationManager.AppSettings["MailHeaderText"];
         static string SiteSearchLink = System.Configuration.ConfigurationManager.AppSettings["SiteSearchLink"];
         static string SearchLinkText = System.Configuration.ConfigurationManager.AppSettings["SearchLinkText"];
-       
+        static string MailSchedule = System.Configuration.ConfigurationManager.AppSettings["SearchLinkText"];
+
+
 
         static IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
 
