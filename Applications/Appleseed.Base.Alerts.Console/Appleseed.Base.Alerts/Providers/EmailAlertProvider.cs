01d7a94d8e1cdaae8e3d3d5780a8b0ebc1f44a07
Eric Ramseur <ramseur@anant.us>
2017-07-23 23:29:36 -0400

    IAlert

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Providers/EmailAlertProvider.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Providers/EmailAlertProvider.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Appleseed.Base.Alerts.Model;
 
 namespace Appleseed.Base.Alerts.Providers
 {
-    class EmailAlertProvider
+    class EmailAlertProvider : IAlert
     {
+
     }
 }
