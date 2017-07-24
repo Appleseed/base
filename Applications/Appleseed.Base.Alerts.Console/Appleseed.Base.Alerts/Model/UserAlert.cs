56581f712b47c480000f889f729400e7afdc6f76
Eric Ramseur <ramseur@anant.us>
2017-07-23 22:37:03 -0400

    add properties

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Model/UserAlert.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Model/UserAlert.cs
@@ -8,5 +8,9 @@ namespace Appleseed.Base.Alerts.Model
 {
     class UserAlert
     {
+        public Guid user_id { get; set; }
+        public string email { get; set; }
+        public string name { get; set; }
+        public string source { get; set; }
     }
 }
