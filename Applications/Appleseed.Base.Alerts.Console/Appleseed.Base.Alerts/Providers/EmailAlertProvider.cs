44a42a4c7d7e9f0f27d4ac8eb6f51c76974ee88e
Eric Ramseur <ramseur@anant.us>
2017-07-23 23:35:18 -0400

    add of generic GetSearchAlertViaSolr

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Providers/EmailAlertProvider.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Providers/EmailAlertProvider.cs
@@ -8,12 +8,16 @@ using System.Data;
 using Dapper;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Appleseed.Base.Alerts.Providers
 {
     class EmailAlertProvider : IAlert
     {
         static IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+        static string SolrURL = System.Configuration.ConfigurationManager.AppSettings["SolrURL"];
 
         public List<UserAlert> GetUserAlertSchedules(string scheudle)
         {
@@ -21,6 +25,29 @@ namespace Appleseed.Base.Alerts.Providers
             commandType: CommandType.StoredProcedure).ToList<UserAlert>();
 
             return userAlerts;
+        }
+
+        public RootSolrObject GetSearchAlertViaSolr(string query)
+        {
+            // perform split function
+
+            string url = SolrURL + WebUtility.HtmlDecode(query);
+            HttpWebRequest getRequest = (HttpWebRequest)WebRequest.Create(url);
+            getRequest.Method = "GET";
+
+            using (var getResponse = (HttpWebResponse)getRequest.GetResponse())
+            {
+                Stream newStream = getResponse.GetResponseStream();
+                StreamReader sr = new StreamReader(newStream);
+
+                var result = sr.ReadToEnd();
+
+                var searchResults = JsonConvert.DeserializeObject<RootSolrObject>(result);
+
+                return searchResults;
+
+            }
+
         }
     }
 }
