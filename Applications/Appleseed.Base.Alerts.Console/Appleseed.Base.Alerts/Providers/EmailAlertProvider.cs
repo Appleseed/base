b0a758253f03cf67ce5f9682ff6f3a3c9625d010
Eric Ramseur <ramseur@anant.us>
2017-07-23 23:30:50 -0400

    add of db connection

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Providers/EmailAlertProvider.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Providers/EmailAlertProvider.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Appleseed.Base.Alerts.Model;
+using System.Data;
 
 namespace Appleseed.Base.Alerts.Providers
 {
     class EmailAlertProvider : IAlert
     {
+        static IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
 
+        static List<UserAlert> GetUserAlertSchedules(string scheudle)
+        {
+            var userAlerts = db.Query<UserAlert>("GetPortalUserAlerts", new { alert_schedule = scheudle },
+            commandType: CommandType.StoredProcedure).ToList<UserAlert>();
+
+            return userAlerts;
+        }
     }
 }
