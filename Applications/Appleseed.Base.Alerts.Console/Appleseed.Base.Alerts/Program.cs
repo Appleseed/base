818bd8dc2b34bb102a08d54ec56eb15cd19f2269
Eric Ramseur <ramseur@anant.us>
2017-07-29 23:52:16 -0400

    format

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Program.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Program.cs
@@ -148,7 +148,7 @@ namespace Appleseed.Base.Alerts
                 {
                     sbHtmlContent.Append("<br/><br/><br/>");
 					if (!String.IsNullOrEmpty(results.response.docs[i].item_type))
-                    sbHtmlContent.Append("<br/><h2>" + Helpers.UppercaseFirst(results.response.docs[i].item_type[0]) + " : " + results.response.docs[i].recall_number[0] + "</h2>");
+						sbHtmlContent.Append("<br/><h2>" + Helpers.UppercaseFirst(results.response.docs[i].item_type[0]) + " : " + results.response.docs[i].recall_number[0] + "</h2>");
                    
 				   sbHtmlContent.Append("<strong>Status: </strong>" + results.response.docs[i].status[0] + "<br/>");
                     sbHtmlContent.Append("<strong>Classification: </strong>" + results.response.docs[i].classification + "<br/>");
