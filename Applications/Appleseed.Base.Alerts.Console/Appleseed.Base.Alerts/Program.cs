45bc6814ddbbcd21c9e79923d2c44407463c7b96
Eric Ramseur <ramseur@anant.us>
2017-07-30 15:06:25 -0400

    TestSearchLink in the code

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Program.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Program.cs
@@ -39,7 +39,7 @@ namespace Appleseed.Base.Alerts
         static string MailSchedule = System.Configuration.ConfigurationManager.AppSettings["MailSchedule"];
 
         static string SiteSearchLink = System.Configuration.ConfigurationManager.AppSettings["SiteSearchLink"];
-        static string SearchLinkText = System.Configuration.ConfigurationManager.AppSettings["TestSearchLink"];
+        static string TestSearchLink = System.Configuration.ConfigurationManager.AppSettings["TestSearchLink"];
 
         static IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
         #endregion
@@ -60,7 +60,7 @@ namespace Appleseed.Base.Alerts
             {
                 RootSolrObject response = GetSearchAlertViaSolr(TestSearchQuery);
 
-                SendAlert(TestEmail, SearchLinkText, response).Wait();
+                SendAlert(TestEmail, TestSearchLink, response).Wait();
             }
             else
             {
