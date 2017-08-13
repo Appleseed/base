2b00f0ebcab0e249ff069dbdb32fcc0d372b662a
Eric Ramseur <ramseur@anant.us>
2017-08-12 23:50:32 -0400

    fix of string for ref

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Program.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Program.cs
@@ -208,7 +208,7 @@ namespace Appleseed.Base.Alerts
                     if (!String.IsNullOrEmpty(results.response.docs[i].entry_number))
                         sbHtmlContent.Append("<strong>Entry Number: </strong> " + results.response.docs[i].entry_number + "<br/>");
                     if (!String.IsNullOrEmpty(results.response.docs[i].rfrnc_doc_id))
-                        sbHtmlContent.Append("<strong>Entry Number: </strong> " + results.response.docs[i].rfrnc_doc_id + "<br/>");
+                        sbHtmlContent.Append("<strong>Refrence Doc ID: </strong> " + results.response.docs[i].rfrnc_doc_id + "<br/>");
 
 
 
