fb90c2bf131e7b096013d9983ec035421b997c4d
Eric Ramseur <ramseur@anant.us>
2017-08-12 22:42:37 -0400

    fix helper bugs

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Controller/Helpers.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Controller/Helpers.cs
@@ -1,14 +1,23 @@
 ﻿using Appleseed.Base.Alerts.Model;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using Dapper;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Appleseed.Base.Alerts.Controller
 {
     class Helpers
     {
+        #region App Config Values
+
+
+        static IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+        #endregion
         #region helpers
         public static string UppercaseFirst(string s)
         {
