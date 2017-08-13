cd11f17261b035bbe31fb10fcf88e968c1f73193
Eric Ramseur <ramseur@anant.us>
2017-08-12 22:18:22 -0400

    add constant fields

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Model/Constants.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Model/Constants.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,26 @@ namespace Appleseed.Base.Alerts.Model
 {
     class Constants
     {
+        #region App Config Values
+        // Test Settings
+        static string Mode = System.Configuration.ConfigurationManager.AppSettings["Mode"];
+        static string TestEmail = System.Configuration.ConfigurationManager.AppSettings["TestEmail"];
+        static string TestSearchQuery = System.Configuration.ConfigurationManager.AppSettings["TestSearchQuery"];
+        static string TestSearchLink = System.Configuration.ConfigurationManager.AppSettings["TestSearchLink"];
+        //SendGridAPI Key
+        static string APIKey = System.Configuration.ConfigurationManager.AppSettings["SendGridAPIKey"];
+        //Mail Settings
+        static string MailFrom = System.Configuration.ConfigurationManager.AppSettings["MailFrom"];
+        static string MailFromName = System.Configuration.ConfigurationManager.AppSettings["MailFromName"];
+        static string MailSubject = System.Configuration.ConfigurationManager.AppSettings["MailSubject"];
+        static string MailHeaderText = System.Configuration.ConfigurationManager.AppSettings["MailHeaderText"];
+        static string MailSchedule = System.Configuration.ConfigurationManager.AppSettings["MailSchedule"];
+        //Search Settings
+        static string SiteSearchLink = System.Configuration.ConfigurationManager.AppSettings["SiteSearchLink"];
+        static string SolrURL = System.Configuration.ConfigurationManager.AppSettings["SolrURL"];
+        static string RefreshQuery = System.Configuration.ConfigurationManager.AppSettings["RefreshQuery"];
+        //Data Settings
+        static IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+        #endregion
     }
 }
