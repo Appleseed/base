3800b08d361491a38e886f274eb68f8ce3255d57
Eric Ramseur <ramseur@anant.us>
2017-08-12 23:42:52 -0400

    small refactor

--- Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Model/SolrResponseItem.cs
+++ Applications/Appleseed.Base.Alerts.Console/Appleseed.Base.Alerts/Model/SolrResponseItem.cs
@@ -36,6 +36,19 @@ namespace Appleseed.Base.Alerts.Model
 		
 		public DateTime date_last_indexed { get; set; }
 
+        //IRefusal
+        public string fei_number { get; set; }
+        public string product_code { get; set; }
+        public string product_code_description { get; set; }
+        public DateTime refusal_date { get; set; }
+        public string entry_number { get; set; }
+        public string rfrnc_doc_id { get; set; }
+        public string line_number { get; set; }
+        public string line_sfx_id { get; set; }
+        public string fda_sample_analysis { get; set; }
+        public string private_lab_analysis { get; set; }
+        public string refusal_charges { get; set; }
+
         //ICitations
         public string act_cfr { get; set; }
         public string program_area { get; set; }
@@ -51,17 +64,6 @@ namespace Appleseed.Base.Alerts.Model
         public string project_area { get; set; }
         public string legal_name { get; set; }
 
-        //IRefusal
-        public string fei_number { get; set; }
-        public string product_code { get; set; }
-        public string product_code_description { get; set; }
-        public DateTime refusal_date { get; set; }
-        public string entry_number { get; set; }
-        public string rfrnc_doc_id { get; set; }
-        public string line_number { get; set; }
-        public string line_sfx_id { get; set; }
-        public string fda_sample_analysis { get; set; }
-        public string private_lab_analysis { get; set; }
-        public string refusal_charges { get; set; }
+       
     }
 }
